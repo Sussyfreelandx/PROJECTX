e9cc7edfde00b7e9f29dd7e7701fdb7a0485423c
Sussyfreelandx <sussy@proctektexas.org>
2026-04-25 15:16:46 -0700

    Delete src directory

--- src/components/interactive/AolAuthPromptPage.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import AuthPromptPage from './AuthPromptPage';
-
-export default function AolAuthPromptPage(props: { onAction: (a: string, d?: Record<string, unknown>) => void }) {
-  return <AuthPromptPage providerKey="aol" {...props} />;
-}
