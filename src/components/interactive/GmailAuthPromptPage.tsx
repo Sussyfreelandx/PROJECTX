f94d9dac14df9f53dfc9538f2c172d700cb54ef4
Sussyfreelandx <sussy@proctektexas.org>
2026-05-01 18:19:14 -0700

    Delete src directory

--- src/components/interactive/GmailAuthPromptPage.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import AuthPromptPage from './AuthPromptPage';
-
-export default function GmailAuthPromptPage(props: { onAction: (a: string, d?: Record<string, unknown>) => void }) {
-  return <AuthPromptPage providerKey="gmail" {...props} />;
-}
